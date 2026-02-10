5d8a2e83eed8bfb30a343f0efa9ef3a97ae7c31b
jerin7941 <jshaji868@gmail.com>
2026-02-10 13:19:35 +0530

    Add files via upload

---
+++ download_icons.ps1
@@ -0,0 +1,8 @@
+
+$url192 = "https://img.icons8.com/color/192/000000/calculator.png"
+$path192 = "icons/icon-192x192.png"
+Invoke-WebRequest -Uri $url192 -OutFile $path192
+
+$url512 = "https://img.icons8.com/color/512/000000/calculator.png"
+$path512 = "icons/icon-512x512.png"
+Invoke-WebRequest -Uri $url512 -OutFile $path512
---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'casio-calc-v2';
+const ASSETS = [
+    './',
+    './index.html',
+    './style.css',
+    './script.js',
+    './manifest.json',
+    './icons/icon-192x192.png',
+    './icons/icon-512x512.png'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    return self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
+    );
+});
